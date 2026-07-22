3676d22766f5e5b0e4fe2ede77eed3fac45dc5cb
Prajwal K <prajwal220pp@gmail.com>
2026-07-22 23:38:45 +0530

    report.sh

---
+++ file_management_report/report.sh
@@ -0,0 +1,55 @@
+===========================================
+Linux File Management System Report
+===========================================
+
+Project Name:
+Linux File Management System
+
+Operating System:
+Red Hat Enterprise Linux
+
+Date:
+22 July 2026
+
+Project Objective:
+To perform file and directory management using Linux commands.
+
+Tasks Performed:
+✔ Created project directory
+✔ Created department folders
+✔ Created files
+✔ Added data to files
+✔ Copied files
+✔ Moved files
+✔ Renamed files
+✔ Searched files using find
+✔ Searched content using grep
+✔ Created soft link
+✔ Created hard link
+✔ Changed permissions using chmod
+✔ Changed ownership using chown
+✔ Created backup using tar
+✔ Compressed backup using gzip
+✔ Checked disk usage using du and df
+
+Commands Used:
+mkdir
+touch
+ls
+pwd
+cp
+mv
+rm
+find
+grep
+chmod
+chown
+ln
+tar
+gzip
+du
+df
+tree
+
+Project Status:
+Completed Successfully
