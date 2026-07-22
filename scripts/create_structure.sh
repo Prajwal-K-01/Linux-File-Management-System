a08b555d0e42034ffc230f2fa681ba4c70054896
Prajwal K <prajwal220pp@gmail.com>
2026-07-22 23:31:30 +0530

    create_structure.sh

---
+++ scripts/create_structure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p HR/Employees
+mkdir -p HR/Policies
+mkdir -p IT/Servers
+mkdir -p IT/Software
+mkdir -p Finance/Budget
+mkdir -p Finance/Invoices
+mkdir -p Sales/Customers
+mkdir -p Sales/Reports
+
+echo "Project directory structure created successfully."
